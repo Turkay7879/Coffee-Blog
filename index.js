dc62afaf505470801556799f59aac2acaa1779d1
Eda İrem Deniz <64044153+iremdnz@users.noreply.github.com>
2021-09-22 01:35:11 +0300

    Brew variety page is added

--- index.js
+++ index.js
@@ -7,12 +7,19 @@ app.listen(port, () => {
     console.log("Helloo");
 });
 
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "/views"));
+
 //views klasörünün yönünü direkt görmesi için
 app.use(express.static(path.join(__dirname, "/views")));
 
 app.get("/", (req, res) => {
-    console.log(__dirname);
+    //console.log(__dirname);
     res.sendFile(path.join(__dirname, "/views/main.html"));
+})
+
+app.get("/ejs", (req, res) => {
+    res.render("brewType");
 })
 
 app.get("/contact", (req, res) => {
