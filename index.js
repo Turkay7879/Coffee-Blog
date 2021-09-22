4c877c7d018e1e5c40ee646cdcfdd17e7a37516f
Eda İrem Deniz <64044153+iremdnz@users.noreply.github.com>
2021-09-22 22:03:33 +0300

    Random brew suggest choices are provided

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const port = 8080;
 const path = require('path');
+const photos = ["../img/brew/v60.jpg", "../img/brew/french_press.jpg", "../img/brew/chemex.jpg", "../img/brew/aeropress.jpg"];
 
 app.listen(port, () => {
     console.log("Helloo");
@@ -13,23 +14,40 @@ app.set("views", path.join(__dirname, "/views"));
 //views klasörünün yönünü direkt görmesi için
 app.use(express.static(path.join(__dirname, "/views")));
 
+const selectImg = (img) => {
+    let count = 0;
+    const randImg = [];
+
+    while (count != 3) {
+        let rand = Math.floor(Math.random() * photos.length);
+        if (photos[rand] !== "../img/brew/" + img + ".jpg" && !randImg.includes(photos[rand])) {
+            randImg.push(photos[rand]);
+            count++;
+        }
+    }
+    return randImg;
+}
+
 app.get("/", (req, res) => {
     res.render("main");
 })
 
 app.get("/brewtype/frenchpress", (req, res) => {
+    const randImg = selectImg("chemex");
     const imgPath = "../img/brew/french_press.jpg";
-    res.render("brewType", { imgPath });
+    res.render("brewType", { imgPath, randImg });
 })
 
 app.get("/brewtype/v60", (req, res) => {
+    const randImg = selectImg("v60");
     const imgPath = "../img/brew/v60.jpg";
-    res.render("brewType", { imgPath });
+    res.render("brewType", { imgPath, randImg });
 })
 
 app.get("/brewtype/chemex", (req, res) => {
+    const randImg = selectImg("chemex");
     const imgPath = "../img/brew/chemex.jpg";
-    res.render("brewType", { imgPath });
+    res.render("brewType", { imgPath, randImg });
 })
 
 app.get("/contact", (req, res) => {
