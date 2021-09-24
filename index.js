7bf9252c4ed57273f6b33b76a8328c92fc81102f
Eda İrem Deniz <64044153+iremdnz@users.noreply.github.com>
2021-09-25 00:40:54 +0300

    Coffee variety page is created

--- index.js
+++ index.js
@@ -2,12 +2,16 @@ const express = require("express");
 const app = express();
 const port = 8080;
 const path = require('path');
-const photos = [{"name" : "V60 Demleme", "path" : "../img/brew/v60.jpg", "route" : "../brewtype/v60" },
-                {"name" : "French Press Demleme", "path" : "../img/brew/french_press.jpg", "route" : "../brewtype/frenchpress"},
-                {"name" : "Chemex Demleme", "path" : "../img/brew/chemex.jpg", "route" : "../brewtype/chemex"},
-                {"name" : "AeroPress Demleme", "path" : "../img/brew/aeropress.jpg", "route" : "../brewtype/aeropress"},
-                {"name" : "Syphon Demleme", "path" : "../img/brew/syphon.jpg", "route" : "../brewtype/syphon"},
-                {"name" : "Moka Pot Demleme", "path" : "../img/brew/mokapot.jpg", "route" : "../brewtype/mokapot"} ];
+const photos = [{ "name": "V60 Demleme", "path": "../img/brew/v60.jpg", "route": "../brewtype/v60" },
+{ "name": "French Press Demleme", "path": "../img/brew/french_press.jpg", "route": "../brewtype/frenchpress" },
+{ "name": "Chemex Demleme", "path": "../img/brew/chemex.jpg", "route": "../brewtype/chemex" },
+{ "name": "AeroPress Demleme", "path": "../img/brew/aeropress.jpg", "route": "../brewtype/aeropress" },
+{ "name": "Syphon Demleme", "path": "../img/brew/syphon.jpg", "route": "../brewtype/syphon" },
+{ "name": "Moka Pot Demleme", "path": "../img/brew/mokapot.jpg", "route": "../brewtype/mokapot" }];
+
+const countryPhoto = [{ "name": "Latin Amerika", "path": "../img/continent/latin.jpg", "route": "../coffeevariety/latinamerica" },
+{ "name": "Afrika", "path": "../img/continent/africa.jpg", "route": "../coffeevariety/africa" },
+{ "name": "Asya Pasifik", "path": "../img/continent/asia.jpg", "route": "../coffeevariety/asia" }]
 
 app.listen(port, () => {
     console.log("Helloo");
@@ -77,6 +81,27 @@ app.get("/brewtype/mokapot", (req, res) => {
     const imgPath = "../img/brew/mokapot.jpg";
     const brewType = photos.find(brew => brew.path === imgPath).name;
     res.render("brewType", { brewType, imgPath, randImg });
+});
+
+app.get("/coffeevariety/latinamerica", (req, res) => {
+    const mainImg = countryPhoto[0];
+    const choice1 = countryPhoto[1];
+    const choice2 = countryPhoto[2];
+    res.render("coffeeVariety", { mainImg, choice1, choice2 });
+});
+
+app.get("/coffeevariety/africa", (req, res) => {
+    const mainImg = countryPhoto[1];
+    const choice1 = countryPhoto[0];
+    const choice2 = countryPhoto[2];
+    res.render("coffeeVariety", { mainImg, choice1, choice2 });
+});
+
+app.get("/coffeevariety/asia", (req, res) => {
+    const mainImg = countryPhoto[2];
+    const choice1 = countryPhoto[1];
+    const choice2 = countryPhoto[0];
+    res.render("coffeeVariety", { mainImg, choice1, choice2 });
 });
 
 app.get("/brewtype", (req, res) => {
