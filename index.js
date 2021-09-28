ec141a637854ae59d8f3ff0421381e75314f4029
Türkay <turkay7879@gmail.com>
2021-09-29 00:48:46 +0300

    Move all data to a json file
    + Add content to ejs pages from data.json from now on (Until a db?)
    + Deleted unused pictures

--- index.js
+++ index.js
@@ -2,16 +2,10 @@ const express = require("express");
 const app = express();
 const port = 8080;
 const path = require('path');
-const photos = [{ "name": "V60 Demleme", "path": "../img/brew/v60.jpg", "route": "../brewtype/v60" },
-{ "name": "French Press Demleme", "path": "../img/brew/french_press.jpg", "route": "../brewtype/frenchpress" },
-{ "name": "Chemex Demleme", "path": "../img/brew/chemex.jpg", "route": "../brewtype/chemex" },
-{ "name": "AeroPress Demleme", "path": "../img/brew/aeropress.jpg", "route": "../brewtype/aeropress" },
-{ "name": "Syphon Demleme", "path": "../img/brew/syphon.jpg", "route": "../brewtype/syphon" },
-{ "name": "Moka Pot Demleme", "path": "../img/brew/mokapot.jpg", "route": "../brewtype/mokapot" }];
+const blogData = require('./views/data/data.json');
 
-const countryPhoto = [{ "name": "Latin Amerika", "path": "../img/continent/latin.jpg", "route": "../coffeevariety/latinamerica" },
-{ "name": "Afrika", "path": "../img/continent/africa.jpg", "route": "../coffeevariety/africa" },
-{ "name": "Asya Pasifik", "path": "../img/continent/asia.jpg", "route": "../coffeevariety/asia" }]
+const photos = blogData.coffees;
+const countryPhoto = blogData.continents;
 
 app.listen(port, () => {
     console.log("Helloo");
@@ -48,69 +42,84 @@ app.get("/brewtype/frenchpress", (req, res) => {
     const brewName = "French Press";
     const randImg = selectImg("french_press");
     const imgPath = "../img/brew/french_press.jpg";
+    const intro = blogData.brewInstructions.frenchPress.intro;
+    const instructions = blogData.brewInstructions.frenchPress.details;
     const brewType = photos.find(brew => brew.path === imgPath).name;
-    res.render("brewType", { brewName, brewType, imgPath, randImg });
+    res.render("brewType", { brewName, brewType, imgPath, randImg, intro, instructions });
 })
 
 app.get("/brewtype/v60", (req, res) => {
     const brewName = "V60";
     const randImg = selectImg("v60");
     const imgPath = "../img/brew/v60.jpg";
+    const intro = blogData.brewInstructions.v60.intro;
+    const instructions = blogData.brewInstructions.v60.details;
     const brewType = photos.find(brew => brew.path === imgPath).name;
-    res.render("brewType", { brewName, brewType, imgPath, randImg });
+    res.render("brewType", { brewName, brewType, imgPath, randImg, intro, instructions });
 })
 
 app.get("/brewtype/chemex", (req, res) => {
     const brewName = "Chemex";
     const randImg = selectImg("chemex");
     const imgPath = "../img/brew/chemex.jpg";
+    const intro = blogData.brewInstructions.chemex.intro;
+    const instructions = blogData.brewInstructions.chemex.details;
     const brewType = photos.find(brew => brew.path === imgPath).name;
-    res.render("brewType", { brewName, brewType, imgPath, randImg });
+    res.render("brewType", { brewName, brewType, imgPath, randImg, intro, instructions });
 })
 
 app.get("/brewtype/aeropress", (req, res) => {
     const brewName = "AeroPress";
     const randImg = selectImg("aeropress");
     const imgPath = "../img/brew/aeropress.jpg";
+    const intro = blogData.brewInstructions.aeropress.intro;
+    const instructions = blogData.brewInstructions.aeropress.details;
     const brewType = photos.find(brew => brew.path === imgPath).name;
-    res.render("brewType", { brewName, brewType, imgPath, randImg });
+    res.render("brewType", { brewName, brewType, imgPath, randImg, intro, instructions });
 });
 
 app.get("/brewtype/syphon", (req, res) => {
     const brewName = "Syphon";
     const randImg = selectImg("syphon");
     const imgPath = "../img/brew/syphon.jpg";
+    const intro = blogData.brewInstructions.syphon.intro;
+    const instructions = blogData.brewInstructions.syphon.details;
     const brewType = photos.find(brew => brew.path === imgPath).name;
-    res.render("brewType", { brewName, brewType, imgPath, randImg });
+    res.render("brewType", { brewName, brewType, imgPath, randImg, intro, instructions });
 });
 
 app.get("/brewtype/mokapot", (req, res) => {
     const brewName = "Moka Pot";
     const randImg = selectImg("mokapot");
     const imgPath = "../img/brew/mokapot.jpg";
+    const intro = blogData.brewInstructions.mokapot.intro;
+    const instructions = blogData.brewInstructions.mokapot.details;
     const brewType = photos.find(brew => brew.path === imgPath).name;
-    res.render("brewType", { brewName, brewType, imgPath, randImg });
+    res.render("brewType", { brewName, brewType, imgPath, randImg, intro, instructions });
 });
 
 app.get("/coffeevariety/latinamerica", (req, res) => {
     const mainImg = countryPhoto[0];
     const choice1 = countryPhoto[1];
     const choice2 = countryPhoto[2];
-    res.render("coffeeVariety", { mainImg, choice1, choice2 });
+    const {title, text} = blogData.variety.latinAmerica;
+    res.render("coffeeVariety", { mainImg, choice1, choice2, title, text });
 });
 
 app.get("/coffeevariety/africa", (req, res) => {
     const mainImg = countryPhoto[1];
     const choice1 = countryPhoto[0];
     const choice2 = countryPhoto[2];
-    res.render("coffeeVariety", { mainImg, choice1, choice2 });
+    const {title, text} = blogData.variety.africa;
+    res.render("coffeeVariety", { mainImg, choice1, choice2, title, text });
 });
 
 app.get("/coffeevariety/asia", (req, res) => {
     const mainImg = countryPhoto[2];
     const choice1 = countryPhoto[1];
     const choice2 = countryPhoto[0];
-    res.render("coffeeVariety", { mainImg, choice1, choice2 });
+    const {title, text} = blogData.variety.asiaPacific;
+    res.render("coffeeVariety", { mainImg, choice1, choice2, title, text });
 });
 
 app.get("/brewtype", (req, res) => {
@@ -123,7 +132,8 @@ app.get("/contact", (req, res) => {
 
 app.post("/contact", (req, res) => {
     const { isim, telefon, email, mesaj } = req.body;
-    res.send("hipiti hopiti post request alindi");
+    // Daha düzgün bir mesaj göster, veya redirect?
+    res.send("Mesajınız alınmıştır. İletişime geçtiğiniz için teşekkürler!");
 });
 
 // Geçersiz bir sayfa açılırsa
